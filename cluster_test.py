f8f1d37e8861dcaea7dda4afb528099f280bfc4b
许润泽 <xurunze@xurunzedeMacBook-Pro.local>
2018-05-14 14:03:03 +0800

    first commit

---
+++ cluster_test.py
@@ -0,0 +1,168 @@
+#coding=utf-8
+from __future__ import division
+from my_cluster import *
+
+import argparse
+
+
+def load_label(testsetDir):
+    alone_cluster_label_cnt = 1000
+    labelDict = {}
+    for dir in os.listdir(testsetDir):
+        if dir.startswith('.'):
+            continue
+        else:
+            currentDir = os.path.join(testsetDir, dir)
+            for fileName in os.listdir(currentDir):
+                if fileName.endswith('.jpg'):
+                    if str(dir) != '-1':
+                        labelDict[fileName] = dir
+                    else:
+                        labelDict[fileName] = str(alone_cluster_label_cnt)
+                        alone_cluster_label_cnt += 1
+    return labelDict
+
+def cluster_and_test_from_video_dir(videoDir, picDir, labelDict, methodList=['DBSCAN']):
+    if methodList[0] == 'API':
+        methodResultDict = {}
+        methodResultDict['API'] = test_former_api(videoDir)
+    else:    
+        methodResultDict = cluster_from_video_dir(videoDir, picDir, methodList)
+    for method in methodResultDict.keys():
+        resultDict = methodResultDict[method]
+        resultClusterDict = make_clusterDict_from_resultDict(resultDict)
+        labelClusterDict = make_clusterDict_from_resultDict(labelDict)
+        
+        f_score = pairwise_f_score(resultClusterDict, labelClusterDict, labelDict)
+        return f_score
+
+def find_max_clustered_num(label, resultClusterDict, labelDict):
+    maxNum = 0
+    for k,cluster in resultClusterDict.items():
+        num = count_label_in_cluster(label, cluster, labelDict)
+        if num > maxNum:
+            maxNum = num
+    return maxNum
+
+def count_label_in_cluster(label, cluster, labelDict):
+    num = 0
+    for x in cluster:
+        if labelDict[x] == label:
+            num += 1
+    return num
+
+def make_clusterDict_from_resultDict(resultDict):
+    clusterDict = {}
+    for key in resultDict.keys():
+        if not clusterDict.has_key(resultDict[key]):
+            clusterDict[resultDict[key]] = []
+        clusterDict[resultDict[key]].append(key)
+    return clusterDict
+
+def find_cluster_most_label(listOfSameCluster, labelDict):
+    _statLabelDict = {}
+    for fileName in listOfSameCluster:
+        if not _statLabelDict.has_key(labelDict[fileName]):
+            _statLabelDict[labelDict[fileName]] = 1
+        else:
+            _statLabelDict[labelDict[fileName]] += 1
+    mostLabel = -1
+    mostLabelNum = -1
+    for k,v in _statLabelDict.items():
+        if v > mostLabelNum:
+            mostLabelNum = v
+            mostLabel = k
+    return mostLabel, mostLabelNum
+
+    
+def test_former_api(videoDir):
+    classCnt = 0
+    resultDict = {}
+    for dirName in os.listdir(videoDir):
+        if str(dirName).startswith('.'):
+            continue
+        currentDir = os.path.join(videoDir, dirName)
+        for fileName in os.listdir(currentDir):
+            if fileName.endswith('.jpg'):
+                resultDict[fileName] = str(classCnt)
+        classCnt += 1
+    return resultDict
+
+def pairwise_f_score(resultClusterDict, labelClusterDict, labelDict):
+    precision = pairwise_precision(resultClusterDict, labelDict) 
+    recall = pairwise_recall(resultClusterDict, labelClusterDict, labelDict)
+    f_score = 2 * precision * recall / (precision + recall)
+    print precision, recall
+    return f_score
+
+def pairwise_precision(resultClusterDict, labelDict):
+    above = 0
+    below = 0
+    for k, cluster in resultClusterDict.items():
+        below += compute_combination(2, len(cluster))
+        above += count_right_pair_in_result_cluster(cluster, labelDict)
+    return above / below
+
+def pairwise_recall(resultClusterDict, labelClusterDict, labelDict):
+    above = 0
+    below = 0
+    for k, cluster in labelClusterDict.items():
+        below += compute_combination(2, len(cluster))
+    for k, cluster in resultClusterDict.items():
+        above += count_right_pair_in_result_cluster(cluster, labelDict)
+    return above / below
+
+def count_right_pair_in_result_cluster(cluster, labelDict):
+    num = 0
+    cntDict = {}
+    for x in cluster:
+        if cntDict.has_key(labelDict[x]):
+            cntDict[labelDict[x]] += 1
+        else:
+            cntDict[labelDict[x]] = 1
+    
+    for k, v in cntDict.items():
+        num += compute_combination(2, v)
+    return num
+
+def compute_combination(upNum, downNum):
+    above = 1
+    below = 1
+    for i in range(upNum):
+        above *= downNum - i
+        below *= upNum - i
+    return above / below
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Clustering and Pairwise F_score Evaluation')
+    parser.add_argument('--method', type=str, required=True, help='DBSCAN, API, AP, RankOrder')
+    parser.add_argument('--labelDir', type=str, required=False, default='test_set', help='Path of labeled pictures')
+    parser.add_argument('--videoDir', type=str, required=True, help='Path of features to be clustered')
+    parser.add_argument('--featureList', type=str, required=True, help='Feature list of feature file name')
+    parser.add_argument('--picDir', type=str, required=True, help='Path of pictures to be clustered')
+    parser.add_argument('--saveResult', type=bool, required=True, help='Whether to save the result pics')
+    parser.add_argument('--saveDir', type=str, required=True, help='Path to save clustered pictures')
+    parser.add_argument('--eps', type=float, required=False, default=None, help='DBSCAN parameter')
+    parser.add_argument('--nProcess', type=int, required=False, default=1, help='Number of processes to read data')
+    args = vars(parser.parse_args())
+
+
+    #labelDict = load_label(args['labelDir'])
+    #f_score = cluster_and_test_from_video_dir('5ab52c0e28734100076d67b9', labelDict, methodList=['API'])
+    #f_score = cluster_and_test_from_video_dir(args['videoDir'], args['picDir'], labelDict, methodList=[args['method']])
+    #print f_score
+    if args['eps'] is None:
+        eps = ''
+    else:
+        eps = str(args['eps'])
+
+    saveDir = args['saveDir']+'_'+eps
+    cluster_from_video_dir(args['videoDir'], args['featureList'], args['picDir'], methodList=[args['method']], 
+                            saveResult=args['saveResult'], saveDir=saveDir, eps=args['eps'], nProcess=args['nProcess'])
+
+    print args['eps']
+    os.system("ls -lR {}|grep \"^-\"|wc -l".format(saveDir))
+    os.system("ls {}|wc -l".format(saveDir))
---
+++ my_cluster.py
@@ -0,0 +1,240 @@
+#coding=utf-8
+from sklearn import cluster
+from scipy.spatial.distance import cosine as ssd_cosine_dist
+import numpy as np
+import scipy.misc as sc
+
+import time
+import os
+import shutil
+
+import urllib2
+from parse_json import convert_json_file_to_npy
+#from demo_noLFW import rankOrder_cluster_format
+
+import multiprocessing
+
+
+FEATURE_DIENSION = 512
+
+def read_txtlist(sourceDir, path):
+    pathList = []
+    with open(path, 'r') as f:
+        aPath = f.readline().strip()
+        while aPath:
+            if aPath.startswith('/'):
+                aPath = aPath[1:]
+            pathList.append(os.path.join(sourceDir, aPath))
+            aPath = f.readline().strip()
+    return pathList
+
+def feature_data_reader(dataPath, featureList):
+    feature_list = None
+    global_pic = None
+    filePathList = read_txtlist(dataPath, featureList)
+    #Use first one to initialize
+    feature_list = np.load(filePathList[0])
+    #Concat else
+    cnt = 0
+    for fileFullPath in filePathList[1:]:    
+        cnt += 1 
+        print cnt
+        featureVec = np.load(fileFullPath)
+        try:
+            feature_list = np.vstack((feature_list, featureVec))
+        except:
+            print feature_list.shape, featureVec.shape, fileFullPath
+    return np.asarray(feature_list), global_pic, filePathList 
+
+def feature_data_reader_fromList(filePathList):
+    name = multiprocessing.current_process().name
+    #Use first one to initialize
+    feature_list = np.load(filePathList[0])
+    print feature_list.shape
+    assert feature_list.shape[0] > 0
+    #Concat else
+    cnt = 0
+    noHeadFilePathList = filePathList[1:]
+    while(cnt < len(noHeadFilePathList)):
+        fileFullPath = noHeadFilePathList[cnt]
+        if cnt%1000 == 0:
+            print "Process", name, "done concating", cnt
+        featureVec = np.load(fileFullPath)
+
+        if len(featureVec.shape)>0:# == 512:
+            feature_list = np.vstack((feature_list, featureVec))
+        else:
+            print 'in', "Process", name
+            print feature_list.shape[0], len(noHeadFilePathList), "Process", name
+            noHeadFilePathList.pop(cnt)
+            print feature_list.shape[0], len(noHeadFilePathList), "Process", name
+            cnt -= 1
+            print feature_list.shape, featureVec.shape, fileFullPath
+        cnt += 1
+
+    print feature_list.shape[0], len(noHeadFilePathList), "Process", name
+    newFilePathList = [filePathList[0]] + noHeadFilePathList
+    print feature_list.shape[0], len(newFilePathList), "Process", name
+    return np.asarray(feature_list), newFilePathList
+
+def multiprocess_feature_data_reader(dataPath, featureList, nProcess=1):
+    if nProcess == 1:
+        return feature_data_reader(dataPath, featureList)
+    else:
+        feature_list = None
+        global_pic = None
+        filePathList = read_txtlist(dataPath, featureList)
+        total_line = len(filePathList)
+        p = multiprocessing.Pool(nProcess)
+        pos = 0
+        step = total_line / nProcess + 1
+        resList = []
+        for i in range(nProcess):
+            if i == nProcess - 1:
+                resList.append(p.apply_async(feature_data_reader_fromList,args=(filePathList[pos:],)))
+            else: 
+                resList.append(p.apply_async(feature_data_reader_fromList,args=(filePathList[pos:pos+step],)))
+                pos += step
+        p.close()
+        p.join()
+        for i in range(nProcess):
+            if i == 0:  
+                feature_list, filePathList = resList[i].get()
+            else:
+                feature_block, filePathList_part = resList[i].get()
+                feature_list = np.vstack((feature_list, feature_block))
+                filePathList = filePathList + filePathList_part
+        return np.asarray(feature_list), global_pic, filePathList 
+
+
+def cluster_face_features(feature_list, method=None, precomputed=True, eps=0.5):
+    if feature_list is not None:
+        face_feature_list = feature_list
+
+    
+    if face_feature_list is None:
+        return None
+        
+    if precomputed:
+        metric_type = 'precomputed'
+        dist_matrix = __compute_pairwise_distance(face_feature_list)
+        dist_matrix = dist_matrix
+    else:
+        metric_type = 'euclidean'
+        dist_matrix = np.vstack(face_feature_list)
+        dist_matrix = None  
+    
+    if method == 'AP':
+        cluster_estimator = cluster.AffinityPropagation(affinity=metric_type, damping=.55, preference=-1) 
+        if precomputed:
+            dist_matrix = -dist_matrix
+    elif method == 'DBSCAN':
+        cluster_estimator = cluster.DBSCAN(metric=metric_type, eps=eps, min_samples=2)
+
+    t0 = time.time()
+    cluster_estimator.fit(dist_matrix)
+    t1 = time.time()
+    
+    t = t1 - t0
+    print 'Clustering takes: %f seconds' % t
+    
+    if hasattr(cluster_estimator, 'labels_'):
+        y_pred = cluster_estimator.labels_.astype(np.int)
+    else:
+        y_pred = cluster_estimator.predict(dist_matrix)
+                
+    return y_pred
+
+def __compute_pairwise_distance(face_feature_list):
+    nsamples = len(face_feature_list)
+    assert(nsamples > 0)
+    dist_matrix = 1 - np.dot(face_feature_list, face_feature_list.T)
+    return dist_matrix
+
+def my_cluster(videoDir, featureList, picDir, method, saveResult=False, saveDir='result', eps=0.5, nReaderProcess=1, nClusterProcess=1, **kwargs):
+
+    resultDict = {}
+    t0 = time.time()
+    print "Start loading data: ", t0
+    #feature_list, global_pic, filePathList = feature_data_reader(videoDir, featureList)
+    feature_list, global_pic, filePathList = multiprocess_feature_data_reader(videoDir, featureList, nReaderProcess)
+
+    t1 = time.time()
+    print "Done loading data. Start clustering: ", t1, "Loading data time cost: ", t1 - t0
+
+    my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult, saveDir, eps)
+    
+    return
+
+def my_cluster_after_read(feature_list, filePathList, picDir, method, saveResult=False, saveDir='result', eps=0.5):
+    t1 = time.time()
+    print feature_list.shape, len(filePathList)
+    assert feature_list.shape[0] == len(filePathList)
+    y_pred = cluster_face_features(feature_list=feature_list, method=method, eps=eps)
+    assert len(y_pred) == len(filePathList)
+    t2 = time.time()
+    print "Done clustering. Start copying result: ", t2, "Clustering time cost", t2 - t1
+
+    if saveResult:
+        #saveDirPrefix = 'result_' + method + videoDir.replace('./', '')
+        saveDirPrefix = saveDir
+        for i in range(len(y_pred)):
+            classDir = saveDirPrefix+'/'+str(y_pred[i])+'/'
+            try:
+                os.makedirs(classDir)
+            except:
+                pass
+            picName = filePathList[i].replace('.npy', '.jpg').split('/')[-1]
+            if picName.startswith('/'):
+                picName = picName[1:]
+            picPath = os.path.join(picDir, picName)
+            try:
+                shutil.copyfile(picPath, classDir+picName)
+            except IOError, e:
+                pass
+    t3 = time.time()
+    print "Done copying: ", t3, "Copying time cost", t3 - t2
+    # print "Exiting..."
+    # exit(0)
+
+    return
+
+
+def cluster_from_video_dir(videoDir, featureList, picDir, methodList=['DBSCAN'], saveResult=False, saveDir='result', eps=0.5, nProcess=1):
+    methodResultDict = {}
+    for method in methodList:
+        t0 = time.time()
+        print "method: " + method
+        print "start time: ", t0
+        # methodResultDict[method] = my_cluster(videoDir, featureList, picDir, method, saveResult, saveDir, eps, nProcess)
+        my_cluster(videoDir, featureList, picDir, method, saveResult, saveDir, eps, nProcess)
+        t1 = time.time()
+        print "end time: ", t1
+        print "time cost: ", t1-t0
+    return methodResultDict
+
+def download_json(httpLink):
+    strHtml = urllib2.urlopen(httpLink).read()
+    with open('extraSample1.json', 'w') as f:
+        f.write(strHtml)
+    return 'extraSample1.json'
+
+def cluster_from_httpLink(httpLink):
+    jsonFile = download_json(httpLink)
+    videoDir = convert_json_file_to_npy(jsonFile)
+    #cluster_from_video_dir(videoDir=videoDir)
+
+def cluster_from_httpLinkList(httpLinkList):
+    for httpLink in httpLinkList:
+        cluster_from_httpLink(httpLink)
+
+if __name__ == "__main__":
+    httpLinkList = ['http://100.100.62.235:8000/v1/videos/5aba6d7a4d7eac0007611734/faces',
+                    #'http://100.100.62.235:8000/v1/videos/5aba6d7a4d7eac0007611736/faces',
+                    #'http://100.100.62.235:8000/v1/videos/5aba6da14d7eac0007611738/faces',
+                    #'http://100.100.62.235:8000/v1/videos/5aba6da94d7eac000761173a/faces',
+                    ]
+    #cluster_from_httpLinkList(httpLinkList)
+    #cluster_from_video_dir(videoDir='5ab52c0e28734100076d67b9', methodList=['DBSCAN'])
+
+
